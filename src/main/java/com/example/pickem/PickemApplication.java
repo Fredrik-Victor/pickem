3687560148760b0854b8c3d2f65eeac7025c38cc
ErikssonF <eriksson.nf@gmail.com>
2022-06-03 09:19:48 +0200

    initial commit

---
+++ src/main/java/com/example/pickem/PickemApplication.java
@@ -0,0 +1,13 @@
+package com.example.pickem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickemApplication.class, args);
+    }
+
+}
